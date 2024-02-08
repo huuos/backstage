0b3791f37c0e27e62324540d69e305aa4fa392bf
huuos <sonnylbui@gmail.com>
2024-02-07 17:28:51 -0700

    restructure init

--- backstage-backend/api/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from accounts.models import Account
-
-#Lead Serializer
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
